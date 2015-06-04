31d4a8e983bb7dda7f6b0b728b0f17493152ff51
Doran Cavett <Dorancavett2@gmail.com>
2015-06-04 15:37:14 +0000

    reverted code

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,15 +1,22 @@
-#class Users::RegistrationsController < Devise::RegistrationsController
-  
- # def create
-  #  super do |resource|
-   #   if params[:plan]
-    #    resource.plan_id = params[:plan] 
-     #   if resource.plan_id == 0
-      #    resource.save #add 'save_with_payment' to add in charging capabilities
-       # else
-        #  resource.save
- #       end
- #   end
- # end
-  
-#end
+class Users::RegistrationsController < Devise::RegistrationsController
+  before_filter :select_plan, only: :new  
+ 
+#  def create
+#    super do |resource|
+#      if params[:plan]
+#        resource.plan_id = params[:plan] 
+#          if resource.plan_id == 1
+#            resource.save_with_payment #add 'save_with_payment' to add in charging capabilities
+#          else
+#            resource.save
+#          end
+#      end
+#   end
+
+  private
+    def select_plan
+      unless params[:plan] && (params[:plan] == '1' || params[:plan] == '2')
+        flash[:notice] = "Please select a plan to sign up"
+        redirect_to_root_url
+    end
+end
