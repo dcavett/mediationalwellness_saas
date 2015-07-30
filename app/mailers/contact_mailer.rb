5b9ebb4006a042f329a5869bb913e7ef38d40e21
Doran Cavett <Dorancavett2@gmail.com>
2015-07-30 20:10:19 +0000

    Changed email address to website owner (Jim)

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-  default to: 'dorancavett2@gmail.com'
+  default to: 'jameskjosephlcsw@gmail.com'
   
   def contact_email(name, email, body)
     @name = name
